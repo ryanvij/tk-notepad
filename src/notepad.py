8f812db62618a442afb97c380499e0d1ee184539
Kevin <ryan.vijay2006@gmail.com>
2021-07-22 21:46:39 +0530

    added save, save as and newfile

--- src/notepad.py
+++ src/notepad.py
@@ -1,18 +1,78 @@
-from tkinter import Tk, Button
+import tkinter.filedialog
+from tkinter import Tk, Button, Frame
 from pathlib import Path
 from tkinter.scrolledtext import ScrolledText
 import os
 
 
+# TODO add open file menu
+# TODO add about page
+
+
+# NotePad class
 class NotePad:
     def __init__(self, width, height):
+
         self.width = width
         self.height = height
         self.root = Tk()
-        self.filename = "Untitled"
-        self.save = Button(self.root, width=7, height=1, text="")
-        self.text_area = ScrolledText(self.root, wrap="word")
 
+        # Setting filename as "Untitled and creating empty file_path variable. To be updated after file is saved. "
+        self.filename = "Untitled"
+        self.file_path = ""
+
+        # Status bar frame located at North-West.
+        self.status_bar = Frame(self.root)
+        self.status_bar.pack(side="top", anchor="nw")
+
+        # Save as Save As.. buttons on frame status_bar
+        self.new = Button(self.status_bar, width=7, height=1, text="New", command=self.new_file)
+        self.open = Button(self.status_bar, width=7, height=1, text="Open", command=self.open_file)
+        self.save_as = Button(self.status_bar, width=7, height=1, text="Save As", command=self.save_as_textfile)
+        self.save = Button(self.status_bar, width=7, height=1, text="Save", command=self.save_existing)
+        self.about_page = Button(self.status_bar, width=7, height=1, text="About", command=self.about_page)
+
+        # text_area using ScrolledText, to facilitate scrolling. wrap="word" and undo=True"
+        self.text_area = ScrolledText(self.root, wrap="word", undo=True)
+
+    def about_page(self):
+        pass
+
+    def open_file(self):
+        pass
+
+    def new_file(self):
+        self.root.destroy()
+        nt = NotePad(800, 600)
+        nt.configure()
+        nt.run()
+
+    # Overwrite existing file.
+    def save_existing(self):
+
+        try:
+            # Opening file with "w" mode.
+            with open(self.file_path, "w") as f:
+                # Truncating file to clear it.
+                f.truncate(0)
+                f.write(self.text_area.get("1.0", tkinter.END))
+        except FileNotFoundError:
+            pass
+
+    # Save as a new text file.
+    def save_as_textfile(self):
+        # Using asksaveasfilename from tkinter.filedialog to save .txt filepath
+        self.file_path = tkinter.filedialog.asksaveasfilename(defaultextension=".txt", filetypes=(("Text File", "*.txt"),))
+        try:
+            # Opening file with "w+" mode.
+            with open(self.file_path, "w+") as f:
+                f.write(self.text_area.get("1.0", tkinter.END))
+
+            # Getting filename from filepath and setting the title.
+            self.filename = os.path.basename(self.file_path)
+            self.root.title(f"{self.filename} - Notepad")
+        except FileNotFoundError:
+            pass
 
     def configure(self):
         # Checks whether if "notepad_icon.ico" exists.
@@ -26,10 +86,14 @@ class NotePad:
         # Geometry and Title
         self.root.geometry(f"{self.width}x{self.height}")
         self.root.title(f"{self.filename} - Notepad")
-        self.save.pack(side="top", anchor="nw")
-        self.text_area.pack(fill="both", expand=True, padx=0, pady=0)
 
-
+        # Packing buttons.
+        self.new.pack(side="left")
+        self.open.pack(side="left")
+        self.save.pack(side="left")
+        self.save_as.pack(side="left")
+        self.about_page.pack(side="left")
+        self.text_area.pack(expand=True, fill="both")
 
     def run(self):
         self.root.mainloop()
