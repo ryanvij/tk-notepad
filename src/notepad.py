1a9e9cf65cc31a160dd89492760ade61c51b793a
Kevin <ryan.vijay2006@gmail.com>
2021-07-24 16:24:02 +0530

    cleaned code and added comments

--- src/notepad.py
+++ src/notepad.py
@@ -4,27 +4,22 @@ from pathlib import Path
 from tkinter import Tk, Button, Frame, Label, filedialog, scrolledtext, constants
 
 
-# TODO clean code
-
-
 # NotePad class
 class NotePad:
     def __init__(self, width, height):
-
-        #
         self.width = width
         self.height = height
-        self.root = Tk()
+        self.root = Tk()  # Initializing root window.
 
         # Setting filename as "Untitled and creating empty file_path variable. To be updated after file is saved. "
         self.filename = "Untitled"
         self.file_path = ""
 
-        # Status bar frame located at North-West.
+        # status_bar frame located at North-West.
         self.status_bar = Frame(self.root)
         self.status_bar.pack(side="top", anchor="nw")
 
-        # Save as Save As.. buttons on frame status_bar
+        # Buttons on status_bar frame.
         self.new = Button(self.status_bar, width=7, height=1, text="New", command=self.new_file)
         self.open = Button(self.status_bar, width=7, height=1, text="Open", command=self.open_file)
         self.save_as = Button(self.status_bar, width=7, height=1, text="Save As", command=self.save_as_textfile)
@@ -34,38 +29,49 @@ class NotePad:
         # text_area using ScrolledText, to facilitate scrolling. wrap="word" and undo=True"
         self.text_area = scrolledtext.ScrolledText(self.root, wrap="word", undo=True)
 
-    def callback(self, url):
+        # Labels on about window.
+        self.info = Label(self.about_page, text="This a Notepad program developed by ryanvij, using Tkinter.")
+        self.github_link = Label(self.about_page, text="Github Repository", fg="blue", font="Verdana 7 underline")
+
+    # Opens github repository on default browser.
+    @staticmethod
+    def callback(url):
         webbrowser.open_new(url)
 
+    # Called when About button is clicked.
     def about_page(self):
         self.about_page = Tk()
         self.about_page.resizable(width=True, height=False)
         self.about_page.title("About")
         self.about_page.geometry("400x300")
-        self.info = Label(self.about_page, text="This a Notepad program developed by ryanvij, using Tkinter.")
+
+        # Packing info and github_link
         self.info.pack()
-        self.github_link = Label(self.about_page, text="Github Repository", fg="blue", font="Verdana 7 underline")
         self.github_link.pack()
+
+        # Binding github_link to github repo.
         self.github_link.bind("<Button-1>", lambda e: self.callback("https://github.com/ryanvij/tk-notepad"))
         self.about_page.mainloop()
 
+    # Opens already existing file.
     def open_file(self):
         self.file_path = filedialog.askopenfilename(defaultextension=".txt", filetypes=(("Text File", "*.txt"),))
         with open(self.file_path, "r+") as f:
             text = f.read()
+
+        # Clear text_are
         self.text_area.delete('1.0', constants.END)
         self.root.title(f"{os.path.basename(self.file_path)} - Notepad")
         self.text_area.insert("1.0", text)
 
+    # Destroy root and create new window.
     def new_file(self):
         self.root.destroy()
         nt = NotePad(800, 600)
-        nt.configure()
         nt.run()
 
     # Overwrite existing file.
     def save_existing(self):
-
         try:
             # Opening file with "w" mode.
             with open(self.file_path, "w") as f:
@@ -90,7 +96,7 @@ class NotePad:
         except FileNotFoundError:
             pass
 
-    def configure(self):
+    def run(self):
         # Checks whether if "notepad_icon.ico" exists.
         if os.path.exists(Path(__file__).parent / "icons" / "notepad_icon.ico"):
             # Sets iconbitmap as notepad_icon.ico
@@ -109,13 +115,13 @@ class NotePad:
         self.save.pack(side="left")
         self.save_as.pack(side="left")
         self.about_page.pack(side="left")
+
+        # Packing text_area.
         self.text_area.pack(expand=True, fill="both")
 
-
-    def run(self):
         self.root.mainloop()
 
 
 notepad = NotePad(800, 600)
-notepad.configure()
 notepad.run()
+
