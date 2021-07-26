98640e9da39a17b215fcd0f876635bd5ea238d05
Kevin <ryan.vijay2006@gmail.com>
2021-07-26 09:59:41 +0530

    setuptools

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 
 setup(name='tk-notepad',
-      version='0.1',
+      version='0.2',
       description='tk-notepad is a notepad program developed with Tkinter, equipped with a simple interface.',
+      long_description_content_type='text/markdown',
+      long_description = (here / 'README.md').read_text(encoding='utf-8'),
       url='https://github.com/ryanvij/tk-notepad',
       author='ryanvij',
       author_email='ryan.vijay2006@gmail.com',
