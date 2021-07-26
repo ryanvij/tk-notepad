7f8c279ce943ed7f4c4649d8b182e27b26e7ffdc
Kevin <ryan.vijay2006@gmail.com>
2021-07-26 15:49:24 +0530

    fixed pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 
 setup(name='tk-notepad',
-      version='0.4',
+      version='0.5',
       description='tk-notepad is a notepad program developed with Tkinter, equipped with a simple interface.',
       long_description_content_type='text/markdown',
       long_description = (here / 'README.md').read_text(encoding='utf-8'),
@@ -17,5 +17,6 @@ setup(name='tk-notepad',
         'console_scripts': ['tk-notepad=tk_notepad.notepad:main']
       },
       packages=['tk_notepad'],
+      include_package_data=True,
       zip_safe=False)
 
